4afaea526539665b14317949379cf16b9309145c
xujihao <xujihao1022@163.com>
2023-10-11 22:06:53 +0800

    给健身房所有物添加外键

--- demo/src/main/java/com/example/demo/entity/Coach.java
+++ demo/src/main/java/com/example/demo/entity/Coach.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class Coach {
     private int id;              // 唯一标识符，主键
+    private int gymId;           // 健身房外键
     private String name;         // 教练的姓名
     private String contactInfo;  // 联系信息
     private String qualifications; // 资质
--- demo/src/main/java/com/example/demo/entity/Equipment.java
+++ demo/src/main/java/com/example/demo/entity/Equipment.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class Equipment {
     private int id;            // 唯一标识符，主键
+    private int gymId;           // 健身房外键
     private String name;       // 健身器材的名称
     private String description;// 健身器材的描述
     private int quantity;      // 数量
